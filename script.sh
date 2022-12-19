b066a5b6e2c7415112b230940b2aa3ff1453e970
samuel-edmund-morgan <samuel.edmund.morgan@gmail.com>
2022-12-19 17:24:09 +0200

    RTF added

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ rm -rf /tmp/rtfArray
 
 regex='Target[[:space:]]*=[[:space:]]*[(\")|('"'"')|(\`)](https?|ftp|file):\/\/[-[:alnum:]\+&@#\/%?=~_|.;]*[-[:alnum:]\+&@#\/%=~_|][(\")|('"'"')|(\`)]'
 replace_with='Target=\"\"'
-
+printf "\n\n\n########################################################-DOCX-#########################################################################\n"
 if [ "${#docx_array[@]}" -gt 0 ]; then
 	for (( i=0; i<${#docx_array[@]}; i++ ))
 	do
@@ -28,21 +28,29 @@ if [ "${#docx_array[@]}" -gt 0 ]; then
    		mkdir -p $document_path/002.Cleaned_Documents/
    		mkdir -p $document_path/001.Original_Documents/
    		rnd_path=$(mktemp -d "$document_path/002.Cleaned_Documents/$document_name".XXXXXX)
-   		unzip $document_fullpath -d $rnd_path
-   		mv $document_fullpath $document_path/001.Original_Documents/$document_name
-   		egrep  -orlZ $regex $rnd_path | xargs -0 sed -i -E "s/$regex/$replace_with/g"
-   		(cd $rnd_path && zip -r ../$document_name .)
+		unzip -q $document_fullpath -d $rnd_path
+		mv $document_fullpath $document_path/001.Original_Documents/$document_name
+                if [[ $(egrep  -orl $regex $rnd_path) ]]; then
+                        egrep  -orlZ $regex $rnd_path | xargs -0 sed -i -E "s/$regex/$replace_with/g"
+			(cd $rnd_path && zip -q -r ../$document_name .)
+                        echo "+++ Cleaning of file $document_name have been completed!"
+                else
+                        echo "--- No URLs were found found in $document_name !"
+                fi
+
+
+   		#egrep  -orlZ $regex $rnd_path | xargs -0 sed -i -E "s/$regex/$replace_with/g"
+   		#(cd $rnd_path && zip -r ../$document_name .)
    		rm -rf $rnd_path
-		echo "_______________________________________________________________________________________________________________________________________"
+		printf "_______________________________________________________________________________________________________________________________________\n"
 
 	done
-echo "#########################################################################################################################################################################"
 fi
 
 
 regex='(https?|ftp|file):\/\/[-[:alnum:]\+&@#\/%?=~_|.;]*[-[:alnum:]\+&@#\/%=~_|]'
 replace_with='DELETED_LINK'
-
+printf "\n\n\n##################################################-RTF-################################################################################\n"
 if [ "${#rtf_array[@]}" -gt 0 ]; then
         for (( i=0; i<${#rtf_array[@]}; i++ ))
         do
@@ -53,29 +61,18 @@ if [ "${#rtf_array[@]}" -gt 0 ]; then
                 mkdir -p $document_path/001.Original_Documents/
                 cp $document_fullpath $document_path/002.Cleaned_Documents/$document_name 
                 mv $document_fullpath $document_path/001.Original_Documents/$document_name
-                egrep  -orlZ $regex $document_path/002.Cleaned_Documents/$document_name | xargs -0 sed -i -E "s/$regex/$replace_with/g"
-		echo "Cleaning of file $document_name have been completed!"
-		echo "_______________________________________________________________________________________________________________________________________"
+		if [[ $(egrep  -orl $regex $document_path/002.Cleaned_Documents/$document_name) ]]; then
+			egrep  -orlZ $regex $document_path/002.Cleaned_Documents/$document_name | xargs -0 sed -i -E "s/$regex/$replace_with/g"
+			echo "+++ Cleaning of file $document_name have been completed!"
+		else
+    			echo "--- No URLs were found found in $document_name !"
+		fi
+		printf "_______________________________________________________________________________________________________________________________________\n"
         done
-echo "########################################################################################################################################################################"
 fi
 
 
 
-#egrep  -orlZ '(https?|ftp|file):\/\/[-[:alnum:]\+&@#\/%?=~_|.;]*[-[:alnum:]\+&@#\/%=~_|]' /home/samuel/Documents/1
-#if [ "${#rtf_array[@]}" -gt 0 ]; then
-#        for (( i=0; i<${#rtf_array[@]}; i++ ))
-#        do
-#                document_fullpath="${rtf_array[i]}"
-#                document_name=$(printf "%q" "$document_fullpath" | rev | cut -d/ -f1 | rev)
-#                document_path=$(dirname "${document_fullpath}")
-#                mkdir -p $document_path/002.Cleaned_Documents/
-#                mkdir -p $document_path/001.Original_Documents/
-#                #rnd_path=$(mktemp -d "$document_path/002.Cleaned_Documents/$document_name".XXXXXX)
-#                #unzip $document_fullpath -d $rnd_path
-#                mv $document_fullpath $document_path/001.Original_Documents/$document_name
-#                egrep  -orlZ $regex $rnd_path | xargs -0 sed -i -E "s/$regex/$replace_with/g"
-#                (cd $rnd_path && zip -r ../$document_name .)
-#                rm -rf $rnd_path
-#        done
-#fi
+echo "##################################################-DOC-################################################################################"
+echo "TODO"
+echo "_______________________________________________________________________________________________________________________________________"
