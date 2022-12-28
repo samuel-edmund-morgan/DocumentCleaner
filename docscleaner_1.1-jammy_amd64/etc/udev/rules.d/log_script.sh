0fd71e1aa1e8d02d0bdaeeaa5f208fd4b5e50f09
samuel-edmund-morgan <samuel.edmund.morgan@gmail.com>
2022-12-28 12:59:46 +0200

    Added Windows policy file, added another log

--- docscleaner_1.1-jammy_amd64/etc/udev/rules.d/log_script.sh
+++ docscleaner_1.1-jammy_amd64/etc/udev/rules.d/log_script.sh
@@ -3,6 +3,10 @@
 LOG_FILE="/var/log/docsclean.log"
 touch "$LOG_FILE"
 chmod 666 "$LOG_FILE"
+#added v1.1
+CLEANED_LOG="/var/log/docsclean_cleaned.log"
+touch "$CLEANED_LOG"
+chmod 666 "$CLEANED_LOG"
 
 idProduct=$2    # ATTRS{idProduct}
 idVendor=$3     # ATTRS{idVendor}
@@ -14,9 +18,14 @@ serial=$6       # ATTRS{serial}
 if [ ! -z "$serial" ]; then
 	if [[ "$1" == "add" ]]; then
         	echo "[$(date +"%T - %d.%m.%y")] : Підключено флеш носій. ID of product is $idProduct , ID of vendor is $idVendor , manufacturer is $manufacturer , product is $product , serial is $serial" >> "$LOG_FILE"
+        	#added v1.1
+        	echo "[$(date +"%T - %d.%m.%y")] : Підключено флеш носій. ID of product is $idProduct , ID of vendor is $idVendor , manufacturer is $manufacturer , product is $product , serial is $serial" >> "$CLEANED_LOG"
+        	
 	fi
 	
 fi
 if [[ "$1" == "remove" ]]; then
         echo "[$(date +"%T - %d.%m.%y")] : Флеш носій було вимкнено!" >> "$LOG_FILE"
+        #added v1.1
+        echo "[$(date +"%T - %d.%m.%y")] : Флеш носій було вимкнено!" >> "$CLEANED_LOG"
 fi
